702c7ec0db9991887360541cf0c44ef92dff30f5
paulaasu <paula.asua1@opendeusto.es>
2022-12-29 14:03:16 +0100

    DAO2

--- StravaServidor/src/domain/Entrenamiento.java
+++ StravaServidor/src/domain/Entrenamiento.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Locale;
 import domain.Usuario;
 import javax.jdo.annotations.PersistenceCapable;
-
+ 
 @PersistenceCapable(detachable = "true")
 public class Entrenamiento {
 	private String titulo;
